72883d824c7d51be803d4553b443a9dc2c0cfa68
Hussein AbdulQohar <aqmhussein@gmail.com>
2024-01-25 18:47:53 +0100

    chore: add border

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,8 +5,8 @@ type ButtonVariant = "solid" | "outline" | "text" | "unstyled";
 
 const VARIANT_MAPS: Record<ButtonVariant, string> = {
   text: "bg-transparent",
-  outline: "border-solid border-2 border-grey-500",
-  solid: "bg-orange text-white shadow-[0px_4px_26px_0px_rgba(249,123,92,0.15)]",
+  outline: "border-solid border border-grey-500",
+  solid: "bg-orange text-white shadow-[0px_4px_26px_0px_rgba(249,123,92,0.15)] border-solid border border-orange",
   unstyled: ''
 };
 
